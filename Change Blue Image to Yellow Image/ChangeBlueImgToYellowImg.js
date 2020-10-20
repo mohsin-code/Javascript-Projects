d9b52bdb7e78a3e98157fb49efad6677e7e05924
Mohsin <59390499+mohsin-code@users.noreply.github.com>
2020-10-20 21:49:37 +0500

    Shades of blue instead of just blue

--- Change Blue Image to Yellow Image/ChangeBlueImgToYellowImg.js
+++ Change Blue Image to Yellow Image/ChangeBlueImgToYellowImg.js
@@ -4,7 +4,7 @@ print(image);
 print("\n\nThe above picture is converted into: ")
 
 for (var pixel of image.values()) {    //Each blue pixel is converted into yellow
-    if (pixel.getGreen() != 255 && pixel.getRed() != 255) {
+    if (pixel.getGreen() < 225 && pixel.getRed() < 225) {
         pixel.setRed(255);
         pixel.setGreen(255);
         pixel.setBlue(0);
