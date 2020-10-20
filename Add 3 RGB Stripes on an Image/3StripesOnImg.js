c484956994855f33c229ff024c593917f011fca4
Mohsin <59390499+mohsin-code@users.noreply.github.com>
2020-10-21 02:42:14 +0500

    Create 3StripesOnImg.js

---
+++ Add 3 RGB Stripes on an Image/3StripesOnImg.js
@@ -0,0 +1,14 @@
+var image = new SimpleImage("android.jpeg");
+print(image);
+
+for(var pixel of image.values()){
+    if(pixel.getX()<(image.getWidth()/3)){
+        pixel.setRed(255);
+    }else if(pixel.getX()<(2*image.getWidth()/3)){
+        pixel.setGreen(255);
+    }else{
+        pixel.setBlue(255);
+    }
+}
+
+print(image);
