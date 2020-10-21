1d2467bac24ba457811cbd104de81e8b1b7a2b33
Mohsin <59390499+mohsin-code@users.noreply.github.com>
2020-10-21 14:44:29 +0500

    Create GreenScreenAlgorithm.js

---
+++ Green Screen Image Effect/GreenScreenAlgorithm.js
@@ -0,0 +1,22 @@
+//Run on https://www.dukelearntoprogram.com//course1/example/index.php
+
+var image = new SimpleImage("drewRobert.png");
+var background = new SimpleImage("dinos.png");
+
+background.setSize(image.getWidth(), image.getHeight());
+
+print(image);
+print(background);
+
+//Adds border on an image
+function greenScreen(image, back){
+    for(var pixel of image.values()){
+        if(pixel.getGreen() > (pixel.getRed() + pixel.getBlue())){
+            image.setPixel(pixel.getX(),pixel.getY(),back.getPixel(pixel.getX(),pixel.getY()));
+        }
+    }
+    return print(image);
+}
+
+print("After the Green Screen Effect, The Image look like: ");
+greenScreen(image, background);
