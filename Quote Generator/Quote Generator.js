1167656c37c2e2f5268d4d94b477ad8d42d31929
Mohsin <59390499+mohsin-code@users.noreply.github.com>
2020-10-23 21:40:30 +0500

    Add some quotes
    Refer to Issue #2

--- Quote Generator/Quote Generator.js
+++ Quote Generator/Quote Generator.js
@@ -1,7 +1,9 @@
 function changeBg() {
     let hexChar = [{ q : "Tomorrow is mystery, yesterday was history but today is a gift. That's why its called a present.", p : "Master Ugwe" } , 
-                    { q : "Qurrrote", p : "Markis" } , 
-                    { q : "Quotdddde", p : "Markus"} ];
+                    { q : "Technology is best when it brings people together.", p : "Matt Mullenweg" } ,  
+                    { q : "It is only when they go wrong that machines remind you how powerful they are.", p : "Clive James" } ,  
+                    { q : "The Web as I envisaged it, we have not seen it yet. The future is still so much bigger than the past.", p : "Tim Berners-Lee" } , 
+                    { q : "It has become appallingly obvious that our technology has exceeded our humanity.", p : "Albert Einstein"} ];
     
     let index = parseInt(Math.random() * hexChar.length);
     
