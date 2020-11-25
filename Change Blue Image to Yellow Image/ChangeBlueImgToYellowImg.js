0c4d331e2c9d044770565b57081a20091a043469
mohsin-code <mohsinshah3171@gmail.com>
2020-11-25 12:47:44 +0500

    Create Upload and MakeBlue function

--- Change Blue Image to Yellow Image/ChangeBlueImgToYellowImg.js
+++ Change Blue Image to Yellow Image/ChangeBlueImgToYellowImg.js
@@ -1,14 +1,17 @@
-var image = new SimpleImage("duke_blue_devil.png");     //The image in the current directory
-document.write(image);
-
-print("\n\nThe above picture is converted into: ")
-
-for (var pixel of image.values()) {    //Each blue pixel is converted into yellow
-    if (pixel.getGreen() < 225 && pixel.getRed() < 225) {
-        pixel.setRed(255);
-        pixel.setGreen(255);
-        pixel.setBlue(0);
-    }
+function Upload(){
+    var file = document.getElementById("imgInput");
+    var image = new SimpleImage(file);     //The image in the current directory
+    image.drawTo("canvas");
 }
 
-document.write(image);
+function makeBlue(){
+    for (var pixel of image.values()) {    //Each blue pixel is converted into yellow
+        if (pixel.getGreen() < 225 && pixel.getRed() < 225) {
+            pixel.setRed(255);
+            pixel.setGreen(255);
+            pixel.setBlue(0);
+        }
+    }
+
+    image.drawTo("canvas");
+}
