bdefd49118b5dd020006d819251d2e4f11dfa1a1
mohsin-code <mohsinshah3171@gmail.com>
2020-11-26 18:36:38 +0500

    use a function for color style DOM

--- Counter/Counter.js
+++ Counter/Counter.js
@@ -1,26 +1,35 @@
 //This the script for Counter.html file
 
+//This is the number <p> tag element object
 var num = document.getElementsByClassName("num")[0];
 
-
-function AddCount(){
-    num.innerHTML++;
+//This functions sets the font color accordinn to value
+function color(){
     if(num.innerHTML < 0){
+        //Red if negative
         num.style.color = "red";
     } else if (num.innerHTML > 0){
+        //Green if positive
         num.style.color = "green";
     } else {
+        //Grey if zero
         num.style.color = "grey";
     }
 }
 
+//THis function increases the number by 1
+function AddCount(){
+    //increment
+    num.innerHTML++;
+    
+    //Call the function above
+    color();
+}
+
 function LowerCount(){
+    //decrement
     num.innerHTML--;
-    if(num.innerHTML < 0){
-        num.style.color = "red";
-    } else if (num.innerHTML > 0){
-        num.style.color = "green";
-    } else {
-        num.style.color = "grey";
-    }
+
+    //Call the same function again
+    color();
 }
